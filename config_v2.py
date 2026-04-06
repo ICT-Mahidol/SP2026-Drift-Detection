f28492e7a420ac300d246a03c6ca4e12e8d7d50c
Zittta <95086761+Zittta@users.noreply.github.com>
2026-04-07 01:43:46 +0700

    config v2 for insect dataset

---
+++ config_v2.py
@@ -0,0 +1,29 @@
+from datasets import (
+    InsectsAbruptBalanced,
+    InsectsGradualBalanced,
+    InsectsIncrementalBalanced,
+)
+from detectors import *
+from optimization.model_optimizer import ModelOptimizer
+from optimization.parameter import Parameter
+
+
+class Configuration:
+    streams = [
+        InsectsAbruptBalanced(),
+        #InsectsGradualBalanced(),
+        #InsectsIncrementalBalanced(),
+    ]
+    n_training_samples = 1000
+    models = [
+        ModelOptimizer(
+            base_model=DiscriminativeDriftDetector2019,
+            parameters=[
+                Parameter("n_reference_samples", values=[250, 500]),
+                Parameter("recent_samples_proportion", values=[1.0]),
+                Parameter("threshold", values=[0.7, 0.8]),
+            ],
+            seeds=None,
+            n_runs=5,
+        ),
+    ]
